2ec3b56f029639bbe49b42dfb5a0fe3ad0889e08
Jim <anagnostou102@yahoo.gr>
2026-07-26 20:39:21 +0300

    Resolve B2 merge conflict

--- storeapi/libs/b2/__init__.py
+++ storeapi/libs/b2/__init__.py
@@ -2,7 +2,9 @@ import logging
 from functools import lru_cache
 
 import b2sdk.v2 as b2
+
 from storeapi.config import config
+
 
 logger = logging.getLogger(__name__)
 
@@ -10,15 +12,17 @@ logger = logging.getLogger(__name__)
 @lru_cache()
 def b2_api():
     logger.debug("Creating and authorizing B2 API")
-    info = b2.InMemoryAccountInfo()
-    b2_api = b2.B2Api(info)
-<<<<<<< HEAD
-    logger.info(config.B2_BUCKET_NAME)
-=======
 
->>>>>>> 30b9f9e64566bd10701d9ee7a8064bc9146992bc
-    b2_api.authorize_account("production", config.B2_KEY_ID, config.B2_APPLICATION_KEY)
-    return b2_api
+    info = b2.InMemoryAccountInfo()
+    api = b2.B2Api(info)
+
+    api.authorize_account(
+        "production",
+        config.B2_KEY_ID,
+        config.B2_APPLICATION_KEY,
+    )
+
+    return api
 
 
 @lru_cache()
@@ -28,12 +32,26 @@ def b2_get_bucket(api: b2.B2Api):
 
 def b2_upload_file(local_file: str, file_name: str):
     api = b2_api()
-    logger.debug(f"Uploading {local_file} to B2 as {file_name}")
-    uploaded_file = b2_get_bucket(api).upload_local_file(
-        local_file=local_file, file_name=file_name
-    )
-    download_url = api.get_download_url_for_fileid(uploaded_file.id_)
+
     logger.debug(
-        f"Uploaded {local_file} to B2 successfully and got download URL {download_url}"
+        "Uploading %s to B2 as %s",
+        local_file,
+        file_name,
     )
+
+    uploaded_file = b2_get_bucket(api).upload_local_file(
+        local_file=local_file,
+        file_name=file_name,
+    )
+
+    download_url = api.get_download_url_for_fileid(
+        uploaded_file.id_
+    )
+
+    logger.debug(
+        "Uploaded %s successfully. URL: %s",
+        local_file,
+        download_url,
+    )
+
     return download_url
